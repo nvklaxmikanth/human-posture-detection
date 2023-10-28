085ca6179967a4d707bf11a68d807ab69c1da940
nvklaxmikanth <nvklaxmikanth@gmail.com>
2023-10-28 22:35:57 +0530

    Human Posture Detection

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
+
+var app = express()
+app.use(express.static('model'))
+app.use(express.json())
+app.use(cors())
+app.use('/model', express.static(__dirname + '/model'))
+
+// app.get('/model/', (req, res)= > {
+
+    //})
+// start server
+
+app.listen(4000, ()=> console.log(`Server started  at 4000`))
